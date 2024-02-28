af6b28cf70594c9707d25646dbe29fdd580a7992
shivamgupta1830 <shivamgupta1830@gmail.com>
2024-02-28 15:22:29 +0530

    about page set

--- src/toggleSidebar.js
+++ src/toggleSidebar.js
@@ -3,11 +3,8 @@ import { getElement } from "./utils.js";
 const toggleNav = getElement(".toggle-nav");
 const sidebarOverlay = getElement(".sidebar-overlay");
 const closeBtn = getElement(".sidebar-close");
-console.log(toggleNav);
-console.log(sidebarOverlay);
 
 toggleNav.addEventListener("click", () => {
-  console.log("hi");
   sidebarOverlay.classList.add("show");
 });
 
